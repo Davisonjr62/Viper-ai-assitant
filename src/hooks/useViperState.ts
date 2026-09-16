86858af40d647bf98f711042388fb84898bfec62
Davisonjr62 <nwankwodavid179@gmail.com>
2026-09-16 02:40:28 -0700

    Add Viper state hook

---
+++ src/hooks/useViperState.ts
@@ -0,0 +1,1 @@
+import{useEffect,useState}from'react';import{ViperStateMachine}from'../core/ViperStateMachine';import type{ViperState}from'../core/types';export function useViperState(){const[s,setS]=useState<ViperState>(ViperStateMachine.getState());useEffect(()=>ViperStateMachine.subscribe(n=>setS(n)),[]);return s}
